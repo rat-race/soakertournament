7a7d180bc6dbf67c2f60b9c5c8a2635201f74ca3
rat-race <getamedic@gmail.com>
2014-02-03 21:03:47 +0000

    Added Code Files
    Hard copied code files from the project folder.

---
+++ PlayerInput (2).js
@@ -0,0 +1,25 @@
+﻿#pragma strict
+
+var speed:float = 10 ;
+var rotSpeed:float = 10000 ;
+var velocity:Vector3 ;
+var rotation:float = 0 ;
+
+function Start () 
+{
+
+}
+
+function Update () 
+{
+
+	velocity = Vector3(0 , 0 , Input.GetAxis("Vertical") * speed) ;
+	rotation = Input.GetAxis("Turn") * rotSpeed ;
+	
+	transform.Translate(velocity * Time.deltaTime);
+	//transform.Rotate(0,rotation * Time.deltaTime,0) ;
+	//transform.RotateAround (Vector3.zero, Vector3.up, 20 * Time.deltaTime);
+	
+	//RotateAround(point: Vector3, axis: Vector3, angle: float): void;
+	transform.RotateAround( collider.bounds.center,Vector3.up,rotation * Time.deltaTime);
+}
