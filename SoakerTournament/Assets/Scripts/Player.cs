67abda09a8a20f450910bb10260ee8e6b3af447d
rat-race <getamedic@gmail.com>
2014-02-24 22:58:51 +0000

    Tidied code
    Tidied code

--- SoakerTournament/Assets/Scripts/Player.cs
+++ SoakerTournament/Assets/Scripts/Player.cs
@@ -9,7 +9,6 @@ public class Player : MonoBehaviour
 
 	private GameObject pPrefab ;
 	private GameObject pObject ;
-	private Component pCameraFollow ;
 
 //	Currently will just create a Penny
 //	public Player()
@@ -22,7 +21,6 @@ public class Player : MonoBehaviour
 	{
 		pPrefab = (GameObject)(Resources.Load("Prefabs/player/Penny")) ;
 		pObject = (GameObject)(Instantiate (pPrefab, new Vector3(5, 10, 5), Quaternion.Euler(Vector3.zero))) ;
-		//pCameraFollow = Camera.current.GetComponent("Smooth Follow");
 		Camera.main.transform.parent = pObject.transform ;
 		Camera.main.transform.localPosition = new Vector3(0,20,-100) ;
 		Spawn () ;
