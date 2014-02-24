90c46e3ae57e5739298359fe46d75fbc9b9b9d98
rat-race <getamedic@gmail.com>
2014-02-24 22:51:54 +0000

    Camera follows player
    Ammended player.cs to make the camera a child of the player.

--- SoakerTournament/Assets/Scripts/Player.cs
+++ SoakerTournament/Assets/Scripts/Player.cs
@@ -9,6 +9,7 @@ public class Player : MonoBehaviour
 
 	private GameObject pPrefab ;
 	private GameObject pObject ;
+	private Component pCameraFollow ;
 
 //	Currently will just create a Penny
 //	public Player()
@@ -21,6 +22,9 @@ public class Player : MonoBehaviour
 	{
 		pPrefab = (GameObject)(Resources.Load("Prefabs/player/Penny")) ;
 		pObject = (GameObject)(Instantiate (pPrefab, new Vector3(5, 10, 5), Quaternion.Euler(Vector3.zero))) ;
+		//pCameraFollow = Camera.current.GetComponent("Smooth Follow");
+		Camera.main.transform.parent = pObject.transform ;
+		Camera.main.transform.localPosition = new Vector3(0,20,-100) ;
 		Spawn () ;
 	}
 
