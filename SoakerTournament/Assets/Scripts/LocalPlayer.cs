93afed415754fc9917cd1086f5f3e70036803ce5
rat-race <getamedic@gmail.com>
2014-03-03 22:29:28 +0000

    Instancing Components in Code
    Started to use AddComponent to add all of the game logic via the
    GameLogic class. Currently the input does not work correctly -
    Attempting to debug

--- SoakerTournament/Assets/Scripts/LocalPlayer.cs
+++ SoakerTournament/Assets/Scripts/LocalPlayer.cs
@@ -1,15 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class LocalPlayer : MonoBehaviour {
+public class LocalPlayer : Player
+{
+	public LocalInput lInput ;
 
-	// Use this for initialization
-	void Start () {
 	
+	public new void Start()
+	{
+		pPrefab = (GameObject)(Resources.Load("Prefabs/player/Penny")) ;
+		pObject = (GameObject)(Instantiate (pPrefab, new Vector3(5, 10, 5), Quaternion.Euler(Vector3.zero))) ;
+		Camera.main.transform.parent = pObject.transform ;
+		Camera.main.transform.localPosition = new Vector3(0,20,-100) ;
+		//lInput = gameObject.AddComponent("LocalInput") as LocalInput ;
+		//gameObject.AddComponent("LocalInput") ;// as LocalInput ;
+		//lInput = gameObject.AddComponent<LocalInput>() ;
+		Spawn () ;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+
+	public new void Spawn()
+	{
+		//lInput = gameObject.AddComponent("LocalInput") as LocalInput ;
+		pObject.transform.position = new Vector3(5,10,5) ;
 	}
 }
