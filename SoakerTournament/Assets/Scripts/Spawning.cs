55604c11b8d9d21f9641776280852bbdc44211c7
rat-race <getamedic@gmail.com>
2014-04-03 20:47:40 +0100

    Why are all the meta files changing??!?!?!?
    These meta files are annoying!

---
+++ SoakerTournament/Assets/Scripts/Spawning.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public struct SpawnPoint
+{
+	public Vector3 location ;
+	public int affinity ;
+}
+
+public class Spawning : MonoBehaviour 
+{
+	private SpawnPoint[] Spawnpoints ;
+	
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+
+	}
+
+	int Load(string mapName)
+	{
+		TextReader tr = new StreamReader(mapName) ;
+		
+		String str = null ;
+
+
+	}
+}
