be54a697d12abb3bf7aeca5ca6a17aeb98f09201
Rogueski <ant.whitlam@gmail.com>
2014-05-29 22:37:37 +0100

    Catching Up

---
+++ SoakerTournament/Assets/Scripts/NetworkMenuScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkMenuScript : MonoBehaviour {
+
+	public GUIStyle customButton;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("escape"))
+						Application.LoadLevel ("Testbed");
+	}
+
+	void OnGUI () 
+	{
+		if (GUI.Button (new Rect (20, 20, 180, 50), "Quick Join", customButton)) {
+			Application.LoadLevel("Testbed");
+		}
+	}
+}
